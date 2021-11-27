5f1ef5894f6a1a51140ad731a801044675816c31
Lazy Coder <87596501+AdityaEXP@users.noreply.github.com>
2021-11-27 15:16:04 +0530

    Create config.php

---
+++ Coinpayments_Config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$public_key = 'your public key';
+$private_key = 'your private key ';
+$ipn_url = 'https://domainname.xyz/coinpayementsIPN/ipn.php'; //Location Of ipn.php in url format
+$password = 'aditya'; //your password for creating address
+
+?>
