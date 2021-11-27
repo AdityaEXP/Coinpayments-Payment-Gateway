2195c5c8820d8107edd9d16beef6c4d70f4dffb7
Lazy Coder <87596501+AdityaEXP@users.noreply.github.com>
2021-11-27 15:49:03 +0530

    Create index.php

---
+++ examples/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$your_server_url = 'https://www.domain.xyz/coinpayementsIPN'; //Where the all coinpayment deposite files will be found
+
+$currency = 'BTC'; //BTC, ETH, LTC, DOGE etc....
+$unique_identifier = '100000'; //Unique ID to identify user it can be telegram id or email etc....
+$password = 'aditya'; //Password setuped in Coinpayments_Config/config.php
+
+$address = file_get_contents("$your_server_url/get_callback_address.php?currency=$currency&unique_identifier=$unique_identifier&password=$password");
+
+print_r($address);
+
+
+?>
