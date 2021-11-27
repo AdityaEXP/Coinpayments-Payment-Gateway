004139a3bf1e00ba1f9c4161ea914b320383303c
Lazy Coder <87596501+AdityaEXP@users.noreply.github.com>
2021-11-27 15:49:33 +0530

    Update app.py

--- examples/app.py
+++ examples/app.py
@@ -6,5 +6,5 @@ currency = 'BTC' #BTC, ETH, LTC, DOGE etc....
 unique_identifier = '100000' #Unique ID to identify user it can be telegram id or email etc....
 password = 'aditya' #Password setuped in Coinpayments_Config/config.php
 
-r = requests.get(f'{your_server_url}/get_callback_address.php?currency={currency}&chat_id={unique_identifier}&password={password}')
+r = requests.get(f'{your_server_url}/get_callback_address.php?currency={currency}&unique_identifier={unique_identifier}&password={password}')
 print(r.text)
